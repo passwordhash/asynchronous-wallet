b238874e1d2fc92475ece748a1d783d1bc49b931
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-07-08 19:45:23 +0300

    Add postgres migrations

---
+++ migrations/postgres/000002_test_data.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO wallets (id, balance) VALUES
+('b1a8c0d2-3f4e-4b5c-8d9e-0f1a2b3c4d5e', 1000),
+('c2b9a8d7-6e5f-4a3b-8c9d-0e1f2a3b4c5d', 2000),
+('d3c8b7a6-5e4f-3a2b-8c9d-0e1f2a3b4c5d', 3000);
